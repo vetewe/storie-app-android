2d538a4d2e38bb0d8d7a0be794c9e2991b401489
vtw <vtiaraw2@gmail.com>
2024-11-27 23:22:06 +0700

    refactor: updated code in main, nav and bottomnav

--- app/src/main/java/com/example/storie/MainActivity.kt
+++ app/src/main/java/com/example/storie/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.storie
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.storie.data.preferences.SettingPreference
+import com.example.storie.data.preferences.dataStore
 import com.example.storie.databinding.ActivityMainBinding
+import com.example.storie.ui.activity.setting.SettingViewModel
+import com.example.storie.ui.activity.setting.SettingViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,17 +23,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val settingPreference = SettingPreference.getInstance(application.dataStore)
+        val settingViewModel = ViewModelProvider(
+            this,
+            SettingViewModelFactory(settingPreference)
+        )[SettingViewModel::class.java]
+
+        settingViewModel.getThemeSettings().observe(this) { darkModeActive ->
+            if (darkModeActive) {
+                delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+            }
+        }
+
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_add, R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
