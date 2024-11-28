87d16e69fbf0230a7a37eb04ecd9c5e3384d30f6
vtw <vtiaraw2@gmail.com>
2024-11-28 20:50:06 +0700

    refactor: update manifest also apiservice

--- app/src/main/java/com/example/storie/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/storie/data/retrofit/ApiService.kt
@@ -21,7 +21,10 @@ interface ApiService {
     ): LoginResponse
 
     @GET("stories")
-    suspend fun getStories(): StoryResponse
+    suspend fun getStories(
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int = 20
+    ): StoryResponse
 
     @GET("stories/{id}")
     suspend fun getDetailStories(
@@ -33,5 +36,12 @@ interface ApiService {
     suspend fun addStories(
         @Part file: MultipartBody.Part,
         @Part("description") description: RequestBody,
+        @Part("lat") lat: RequestBody?,
+        @Part("lon") lon: RequestBody?
     ): AddStoryResponse
+
+    @GET("stories")
+    suspend fun getStoriesWithLocation(
+        @Query("location") location: Int = 1
+    ): StoryResponse
 }
